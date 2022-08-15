66412e788e7ff7c03c1fa9dbfce8e8cb112311db
Vishal Vashisht <58656490+VishalVashisht@users.noreply.github.com>
2022-08-15 09:40:11 +0530

    Update pairSum.java

--- Lecture 9 : Arrays 1/pairSum.java
+++ Lecture 9 : Arrays 1/pairSum.java
@@ -74,7 +74,7 @@ public class pairSum {
 		int count = 0;
 		
 		for(int i=0; i<arr.length; i++) {
-			for(int j=i; j<arr.length; j++) {
+			for(int j=i+1; j<arr.length; j++) {
 				if(arr[i] + arr[j] == x) {
 					count++;
 				}
